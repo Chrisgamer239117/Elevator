3c7adb6cf7370fd49d6a7267009be0fcfe1e16c8
Chrisgamer239117 <60674019+Chrisgamer239117@users.noreply.github.com>
2020-11-04 11:11:19 -0600

    Add files via upload

--- Elevador.js
+++ Elevador.js
@@ -1,51 +1,48 @@
 var Elevador=(function () {
-    var pisoactual = 7;
-    var control = 0;//0 puerta cerrada, 1 puerta abierta
-    var pisoelegido = 1;
- 
-    
-     function estado(){
-         if(control==0){
-             control=1;
-         }
-         else{
-             for(segundos=0;segundos<=20;segundos++){
-                 control = 0;
+        var pisoactual = 8;
+        var control = 0;//0 puerta cerrada, 1 puerta abierta
+        var pisoelegido = 5;
+     
+         function estado(){
+             if(control==0){
+     
+                 control=1;
+                 console.log("puerta abierta: "+ control);
+     
+                 for(segundos=0;segundos <= 20;segundos++){
+                     control=0; 
+                     console.log("puerta cerrada: "+ control);
+                 }
              }
+     
          }
- 
-     }
-     console.log("Su piso inicial es:"+pisoactual);
-    console.log("Su piso elegido es: "+pisoelegido);
-    
-        if (pisoactual < pisoelegido){
-            for (i=pisoactual; i!=pisoelegido; i++){
-                pisoactual++;
-                console.log(pisoactual);
-                
-            }
-        }
-        else{
-            for (i =pisoactual; i!=pisoelegido; i--){
-                pisoactual--;
-                console.log(pisoactual);
-            }
-        }
-        
-     console.log("A llegado a su piso: "+pisoactual);
- 
- 
- 
- 
- 
-     function Iniciar(){
-         estado();
-         estado();
-         
-         
-     }
-     return Iniciar;
-   }
- 
- )();
- 
+     
+     
+         console.log("Su piso inicial es:"+pisoactual);
+         console.log("Su piso elegido es: "+pisoelegido);
+     
+         function DireccionElevador(){
+             if (pisoactual < pisoelegido){
+                 for (i=pisoactual; i != pisoelegido; i++){
+                     pisoactual++;
+                     console.log(pisoactual);
+     
+                 }
+             }
+             else{
+                 for (i = pisoactual; i != pisoelegido; i--){
+                     pisoactual--;
+                     console.log(pisoactual);
+                 }
+             }
+             console.log("A llegado a su piso: "+pisoactual);
+         }
+     
+     
+         return{
+             estados:estado(),
+             Direccion:DireccionElevador(),
+             estado:estado()
+         }
+     
+     })();
